f27bb77d7ebf6ec2f5f774bb22b08dd7a0e78a80
u-senanayake <mailtoudayanga@gmail.com>
2021-01-26 17:41:37 +0900

    Added owner list controller to display owner list

--- pet-clinic-web/src/main/java/udayanga/senanayake/sfgpetclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/udayanga/senanayake/sfgpetclinic/controllers/OwnerController.java
@@ -1,15 +1,23 @@
 package udayanga.senanayake.sfgpetclinic.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import udayanga.senanayake.sfgpetclinic.services.OwnerService;
 
 @RequestMapping("/owners")
 @Controller
 public class OwnerController {
 
-    @RequestMapping({"", "/", "/index", "/index.html"})
-    public String listOwners() {
+    private final OwnerService ownerService;
 
+    public OwnerController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
+
+    @RequestMapping({"", "/", "/index", "/index.html"})
+    public String listOwners(Model model) {
+        model.addAttribute("owners", ownerService.findAll());
         return "owners/index";
     }
 }
